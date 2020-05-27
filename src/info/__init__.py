65f68e62ba7fe1647e158f0902360037b2dda0b8
Antonio Paya <antonioalfa22@gmail.com>
2020-05-27 12:35:33 +0200

    Change Knock

--- src/info/__init__.py
+++ src/info/__init__.py
@@ -1,31 +1,36 @@
 from src.info.worker_api_client import WorkerAPIClient
-from src.info.knock import knock
+from src.common.command import run_command
 from multiprocessing import Pool
+
 
 def info(args):
     hosts = args.hosts
     audit = args.audit
     for host in hosts:
-        knock(host=host, ports=[7001,8002,9003])
+        knock(host=host, ports=[7001, 8002, 9003])
     if audit:
         pool = Pool(4)
         pool.map(audit_worker, hosts)
-    for x in get_info(hosts): print(x)
+    for x in get_info(hosts):
+        print(x)
     for host in hosts:
-        knock(host=host, ports=[9003,8002,7001])
+        knock(host=host, ports=[9003, 8002, 7001])
+
 
 def audit_worker(host):
     api = WorkerAPIClient(host=host)
     try:
-        print("Host: ",host,", Score: ",api.get_lynis_score())
+        print("Host: ", host, ", Score: ", api.get_lynis_score())
     except:
         print("Error on connect to host: ", host)
+
 
 def get_info(hosts):
     pool = Pool(4)
     result = pool.map(info_worker, hosts)
     worker_info = [x for x in result if result is not None]
     return worker_info
+
 
 def info_worker(host):
     api = WorkerAPIClient(host=host)
@@ -34,3 +39,7 @@ def info_worker(host):
     except:
         print("Error on connect to host: ", host)
         return None
+
+
+def knock(host, ports):
+    run_command('knock ' + str(host) + ' '.join(str(port) for port in ports))
