0e7231178cd70894517e15875b16beabc5ff7684
Antonio Paya <antonioalfa22@gmail.com>
2020-06-05 11:03:02 +0200

    Added hosts support

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -3,6 +3,6 @@ def config(args):
         with open("/etc/ansible/hosts", "w") as f:
             f.write("[{}]".format(args.group))
             for host in args.hosts:
-                f.write(host)
+                f.write("{} ansible_ssh_user=root".format(host))
         with open("../common/hosts", "w") as f:
             f.write(args.group)
