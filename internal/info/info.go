a23e085d13bef9dd5663a069fb1e6b2b0b3ffa90
Antonio Paya <antonioalfa22@gmail.com>
2021-03-31 13:13:25 +0200

    Updated hostsgroups

--- internal/info/info.go
+++ internal/info/info.go
@@ -165,6 +165,7 @@ func GetLynisScore(hosts []string) ([]Result, error) {
 		r := Result{Host: h, Lines: []string{}}
 		client := CreateHardeningClient(h)
 		res, err := client.GetLynisScore(ctx, &grpc.ScoreReq{})
+		fmt.Println(res)
 		if err != nil {
 			return nil, errors.New("Error: can not connect to host " + h + " on port 8128")
 		} else {
